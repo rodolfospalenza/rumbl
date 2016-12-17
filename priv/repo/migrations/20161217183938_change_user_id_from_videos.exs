f7069e6af69fdf2a1271b0892a4f64718851a728
Rodolfo Spalenza <rodolfo.spalenza@gmail.com>
2016-12-17 16:52:19 -0200

    Migration to change on_delete videos table.

---
+++ priv/repo/migrations/20161217183938_change_user_id_from_videos.exs
@@ -0,0 +1,19 @@
+defmodule Rumbl.Repo.Migrations.ChangeUserIdFromVideos do
+  use Ecto.Migration
+
+  def up do
+    execute "ALTER TABLE videos DROP CONSTRAINT videos_user_id_fkey"
+
+    alter table(:videos) do
+      modify :user_id, references(:users, on_delete: :delete_all)
+    end
+  end
+
+  def down do
+    execute "ALTER TABLE videos DROP CONSTRAINT videos_user_id_fkey"
+
+    alter table(:videos) do
+      modify :user_id, references(:users, on_delete: :nothing)
+    end
+  end
+end
